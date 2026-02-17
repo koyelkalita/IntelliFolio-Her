4af816e04359714df93203336e8ea2234d8cdd3b
Sania Siddiqui <sania.siddiqui231@gmail.com>
2026-02-17 23:39:41 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,1 +1,16 @@
+export async function generatePortfolio(data) {
+  try {
+    const res = await fetch("http://localhost:5000/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    return await res.json();
+  } catch (err) {
+    return { status: "error", message: "Server error" };
+  }
+}
 
